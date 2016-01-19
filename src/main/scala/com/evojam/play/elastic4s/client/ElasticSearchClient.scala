7e45e055ad61900f63a00560335fda41a886a6ff
Wojciech Ptak <wojtek@evojam.com>
2016-01-19 14:54:55 +0100

    Collapsed update and insert into upsert

--- src/main/scala/com/evojam/play/elastic4s/client/ElasticSearchClient.scala
+++ src/main/scala/com/evojam/play/elastic4s/client/ElasticSearchClient.scala
@@ -4,50 +4,62 @@ import scala.concurrent.{ExecutionContext, Future}
 
 import play.api.libs.json.Writes
 
+import com.sksamuel.elastic4s.SearchDefinition
+
+import com.evojam.play.elastic4s.core.search.PreparedSearch
+
 trait ElasticSearchClient {
 
   /**
-   * Index a document
-   * https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html
+   * Indexes or updates a document.
    *
-   * @param indexName name of the index to
+   * Performs an ES Index request. If a document with given ID exists, it will be overwritten.
+   *
+   * @see [[https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html]]
+   *
+   * @param indexName name of the index
    * @param doctype type of the document
    * @param id id to be assigned to the document
    * @param doc the document to be indexed
    * @param exc the execution context
    * @tparam T type of the document, must have an implicit Writes[T] available in scope
-   * @return Future[Boolean] whose value depends upon the success of the indexing operation
+   * @return Future[Boolean] resolved with `true` if the operation succeeded
    */
-  def indexDocument[T](indexName: String, doctype: String, id: String, doc: T)(
-    implicit w: Writes[T], exc: ExecutionContext): Future[Boolean]
-
+  def upsert[T: Writes](indexName: String, doctype: String, id: String, doc: T)
+      (implicit exc: ExecutionContext): Future[Boolean]
 
   /**
-   * Update a document
-   * https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-update.html
+   * Removes a document with the given ID.
    *
-   * @param indexName name of the index to
-   * @param doctype type of the document
-   * @param id id to be assigned to the document
-   * @param doc the document to be indexed
-   * @param exc the execution context
-   * @tparam T type of the document, must have an implicit Writes[T] available in scope
-   * @return Future[Boolean] whose value depends upon the success of the update operation
-   */
-  // scalastyle:off method.length
-  def updateDocument[T](indexName: String, doctype: String, id: String, doc: T)(
-    implicit w: Writes[T], exc: ExecutionContext): Future[Boolean]
-
-  /**
-   * Remove a document
-   * https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-delete.html
+   * @see [[https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-delete.html]]
    *
    * @param indexName name of the index to
    * @param doctype type of the document
    * @param id id to be assigned to the document
    * @param exc the execution context
-   * @return Future[Boolean] whose value depends upon the success of the remove operation
+   * @return Future[Boolean] resolved with `true` if the document was deleted successfully, `false` if it
+   *         couldn't be found.
    */
-  def removeDocument(indexName: String, doctype: String, id: String)(
-    implicit exc: ExecutionContext): Future[Boolean]
+  def remove(indexName: String, doctype: String, id: String)(implicit exc: ExecutionContext): Future[Boolean]
+
+  /**
+   * Prepares a search query for execution.
+   *
+   * @param searchDef the search definition constructed using elastic4s DSL
+   * @return a [[com.evojam.play.elastic4s.core.search.PreparedSearch]] instance encapsulating the query
+   * @example
+   * {{{
+   * import play.api.libs.Json
+   *
+   * case class City(name: String, population: Long)
+   * object City {
+   *   implicit val format = Json.format[City]
+   * }
+   *
+   * val elastic: ElasticClient = ...
+   * val myQuery: SearchDefinition = search in "places" -> "cities" query "London"
+   * val result: Future[List[City]] = elastic.search(myQuery).collect[City]
+   * }}}
+   */
+  def search(searchDef: SearchDefinition): PreparedSearch
 }
--- src/main/scala/com/evojam/play/elastic4s/client/ElasticSearchClientImpl.scala
+++ src/main/scala/com/evojam/play/elastic4s/client/ElasticSearchClientImpl.scala
@@ -1,93 +1,54 @@
 package com.evojam.play.elastic4s.client
 
-import scala.concurrent.Future.successful
 import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future.successful
 import scala.language.implicitConversions
 
 import play.api.Logger
 import play.api.libs.json._
 
 import org.elasticsearch.action.ActionResponse
+import org.elasticsearch.action.index.IndexResponse
 
 import com.google.inject.Inject
-import com.sksamuel.elastic4s.ElasticDsl._
-import com.sksamuel.elastic4s.source.DocumentSource
 import com.sksamuel.elastic4s.{ElasticClient, SearchDefinition}
+import com.sksamuel.elastic4s.ElasticDsl._
+import com.sksamuel.elastic4s.source.{DocumentSource, JsonDocumentSource}
 
 import com.evojam.play.elastic4s.core.search.PreparedSearch
 
-class ElasticSearchClientImpl @Inject() (val underlying: ElasticClient) extends ElasticSearchClient {
+class ElasticSearchClientImpl @Inject() (val client: ElasticClient) extends ElasticSearchClient {
 
-  val logger = Logger(getClass)
-
-  private case class JsValueSource(js: JsValue) extends DocumentSource {
-    override def json = js.toString()
-  }
-
-  private implicit def toSource[T <: JsValue](js: T): DocumentSource = JsValueSource(js)
+  private[this] val logger = Logger(getClass)
 
   private def getHeaders[R <: ActionResponse](response: R) =
     Option(response.getHeaders)
       .map(_.toString)
       .getOrElse("<null>")
 
-  private def executeInsert(indexName: String, doctype: String, id: String, in: DocumentSource)
-      (implicit exc: ExecutionContext) =
-    underlying.execute {
+  private def executeUpsert(indexName: String, doctype: String, id: String, in: DocumentSource)
+      (implicit exc: ExecutionContext): Future[Option[IndexResponse]] =
+    client.execute {
       logger.debug(s"Index into index=$indexName type=$doctype doc=${in.json}")
       index.into(indexName -> doctype)
         .doc(in)
         .id(id)
-    }
+    } map Option.apply
 
-  /**
-   * Builds a SearchBuilder using the provided SearchDefinition that can be executed using the `SearchBuilder.collect`
-   * method
-   *
-   * @param searchDef the search definition to wrap
-   * @return SearchBuilder that encapsulates the SearchDefinition provided
-   *
-   * {{
-   * import play.api.libs.Json
-   *
-   * case class City(name: String, population: Long)
-   * object City {
-   *   implicit val format = Json.format[City]
-   * }
-   *
-   * val myQuery: SearchDefinition = search in "places" -> "cities" query "London"
-   *
-   * val result: Future[List[City]] =
-   *   search(myQuery)
-   *     .collect[City]
-   * }}
-   */
-  def search(searchDef: SearchDefinition)(implicit exc: ExecutionContext): PreparedSearch =
-    PreparedSearch(searchDef, underlying)
+  def search(searchDef: SearchDefinition): PreparedSearch =
+    PreparedSearch(searchDef, client)
 
-  /**
-   * Index a document
-   * https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html
-   *
-   * @param indexName name of the index to
-   * @param doctype type of the document
-   * @param id id to be assigned to the document
-   * @param doc the document to be indexed
-   * @param exc the execution context
-   * @tparam T type of the document, must have an implicit Writes[T] available in scope
-   * @return Future[Boolean] whose value depends upon the success of the indexing operation
-   */
-  override def indexDocument[T](indexName: String, doctype: String, id: String, doc: T)
-      (implicit w: Writes[T], exc: ExecutionContext): Future[Boolean] = {
+
+  def upsert[T: Writes](indexName: String, doctype: String, id: String, doc: T)
+      (implicit exc: ExecutionContext): Future[Boolean] = {
     require(indexName != null, "indexName cannot be null")
     require(doctype != null, "doctype cannot be null")
     require(id != null, "id cannot be null")
     require(doc != null, "doc cannot be null")
 
-    w.writes(doc) match {
+    Json.toJson(doc) match {
       case json: JsObject =>
-        executeInsert(indexName, doctype, id, json).map(Option(_))
-          .map {
+        executeUpsert(indexName, doctype, id, JsonDocumentSource(Json.stringify(json))).map {
           case Some(response) if response.isCreated =>
             logger.info("Document has been indexed (create), retrieving headers= " + getHeaders(response))
             true
@@ -104,77 +65,16 @@ class ElasticSearchClientImpl @Inject() (val underlying: ElasticClient) extends 
     }
   }
 
-  private def executeUpdate(indexName: String, doctype: String, id: String, in: DocumentSource)
-      (implicit exc: ExecutionContext) =
-    underlying.execute {
-      logger.debug(s"Update document index=$indexName type=$doctype id=$id doc=${in.json}")
-      update
-        .id(id)
-        .in(indexName -> doctype)
-        .doc(in)
-    }
-
-  /**
-   * Update a document
-   * https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-update.html
-   *
-   * @param indexName name of the index to
-   * @param doctype type of the document
-   * @param id id to be assigned to the document
-   * @param doc the document to be indexed
-   * @param exc the execution context
-   * @tparam T type of the document, must have an implicit Writes[T] available in scope
-   * @return Future[Boolean] whose value depends upon the success of the update operation
-   */
-  // scalastyle:off method.length
-  override def updateDocument[T](indexName: String, doctype: String, id: String, doc: T)
-      (implicit w: Writes[T], exc: ExecutionContext): Future[Boolean] = {
-    require(indexName != null, "indexName cannot be null")
-    require(doctype != null, "doctype cannot be null")
-    require(id != null, "id cannot be null")
-    require(doc != null, "doc cannot be null")
-
-    w.writes(doc) match {
-      case json: JsObject =>
-        executeUpdate(indexName, doctype, id, json).map(Option(_))
-          .map {
-          case Some(response) if response.isCreated =>
-            logger.info("Document has been updated, retrieving headers=" + getHeaders(response))
-            true
-          case Some(response) =>
-            logger.info("Document has been updated, retrieving headers=" + getHeaders(response))
-            true
-          case None =>
-            logger.error("Elastic execute update result IndexResponse is null, " +
-              "Treating it as an error but can do nothing")
-            false
-        }
-      case _ =>
-        logger.error("Refuse to update document that is not a JsObject")
-        successful(false)
-    }
-  }
-  // scalastyle:on
 
   private def executeRemove(indexName: String, doctype: String, id: String)(implicit exc: ExecutionContext) =
-    underlying.execute {
+    client.execute {
       logger.debug(s"Remove document from index=$indexName type=$doctype id=$id")
       delete
         .id(id)
         .from(indexName -> doctype)
     }
 
-  /**
-   * Remove a document
-   * https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-delete.html
-   *
-   * @param indexName name of the index to
-   * @param doctype type of the document
-   * @param id id to be assigned to the document
-   * @param exc the execution context
-   * @return Future[Boolean] whose value depends upon the success of the remove operation
-   */
-  override def removeDocument(indexName: String, doctype: String, id: String)
+  override def remove(indexName: String, doctype: String, id: String)
       (implicit exc: ExecutionContext): Future[Boolean] = {
     require(indexName != null, "indexName cannot be null")
     require(doctype != null, "doctype cannot be null")
