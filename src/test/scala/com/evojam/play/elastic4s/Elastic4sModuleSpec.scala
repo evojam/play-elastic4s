7652c429e5aa07c8fbe9d274f31d439ca7d8d0e6
Wojciech Ptak <wojtek@evojam.com>
2016-01-25 16:28:11 +0100

    Inject index types

--- src/test/scala/com/evojam/play/elastic4s/Elastic4sModuleSpec.scala
+++ src/test/scala/com/evojam/play/elastic4s/Elastic4sModuleSpec.scala
@@ -20,9 +20,20 @@ class Elastic4sModuleSpec extends Specification {
         "yourCluster" -> Map(
           "default" -> true,
           "uri" -> "elasticsearch://10.10.0.2:9300",
-          "cluster.name" -> "your-cluster")))
+          "cluster.name" -> "your-cluster"),
+        "indexTypes" -> Map(
+          "book" -> Map(
+            "index" -> "library",
+            "type" -> "book"
+          ),
+          "author" -> Map(
+            "index" -> "library",
+            "type" -> "author"
+      ))))
 
       module.buildSetup(conf) must haveLength(2)
+      module.IndexTypeBindingBuilder(conf).bindings must haveLength(2)
+
 
     }
   }
